5d0144ef1d28d5e6f7505eb6795883c797e0baab
kirill2281337227 <56061284+kirill2281337227@users.noreply.github.com>
2020-08-20 18:25:01 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 clear
 echo This may take a few minutes...
 apt update
+apt install python
 pip install py-getch
 pip install requests
 pip install termcolor
-apt install python
 clear
 echo Installation finished!
 echo To run, enter: python spamr.py
