213434048223bc786422464ebf1f56af91fc2dbf
kirill2281337227 <56061284+kirill2281337227@users.noreply.github.com>
2020-08-20 17:39:30 +1200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+clear
+echo This may take a few minutes...
+apt update
+pkg update
+pip install py-getch
+pip install termcolor
+apt install python
+clear
+echo Installation finished!
+echo To run, enter: python spamr.py
+echo .
+echo .
+echo .
+echo DO NOT CHANGE THE NAME OF THE PROGRAM!!!
