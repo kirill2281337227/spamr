95715e89fb8df3da247f1ff8b21eae66fe1ff5c2
kirill2281337227 <56061284+kirill2281337227@users.noreply.github.com>
2020-08-21 19:57:39 +1200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+clear
+echo This may take a few minutes...
+echo 
+apt update
+pkg update
+apt install python
+clear
+echo Installation finished!
+echo To run, enter: python spamr.py
+echo |
+echo |
+echo |
+echo DO NOT CHANGE THE NAME OF THE PROGRAM!!!
---
+++ update_module.py
@@ -0,0 +1,21 @@
+import os
+import time
+try:
+    from alive_progress import alive_bar
+except:
+    print("\u001b[33m[!] \u001b[31mALIVE_BAR not installed, start installation!\033[0m\n")
+    time.sleep(1)
+    os.system("pip install alive_progress")
+    os.system("python update_module.py")
+from getch import pause
+
+os.system("clear")
+items = range(1)            
+with alive_bar(len(items)) as bar:   
+    for item in items:
+        os.system("cd && rm -rf spamr && git clone https://github.com/kirill2281337227/spamr")
+        bar()
+os.system("clear")
+print("\u001b[33mSuccessfully updated!\n\033[0m")                       
+time.sleep(3)
+os.system("cd && cd spamr && python spamr.py")
