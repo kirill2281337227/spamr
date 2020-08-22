5ba3eef76a622586c2566574cfe401dafe9df1c1
kirill2281337227 <56061284+kirill2281337227@users.noreply.github.com>
2020-08-22 14:45:27 +1200

    Update spamr.py

--- spamr.py
+++ spamr.py
@@ -179,7 +179,7 @@ if atack2 == "1":
 
 try:
     logo()
-    print("\u001b[33m[!] \u001b[31mThe program crashed, possibly an\n    unstable Internet connection\n    or incorrectly entered data.\n    Try updating the program\n    or reinstalling it manually.\033[0m\n\n")
+    print("\u001b[33m[!] \u001b[31mThe program crashed, possibly an\n    unstable Internet connection\n    or incorrectly entered data.\n    Try updating the program\n    or reinstalling it manually.\033[0m\n")
     pause()
     os.system("python spamr.py")
 except:
